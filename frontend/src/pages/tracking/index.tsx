5b0c2573e3ffb3513734cd6272a65519bb17b198
Jan van Brügge <supermanitu@gmail.com>
2020-05-28 15:20:05 +0200

    Add simple buttons for tracking

--- frontend/src/pages/tracking/index.tsx
+++ frontend/src/pages/tracking/index.tsx
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
-import logo from "../../logo.svg";
+import "./tracking.scss";
 import "../../App.scss";
+import {
+  Card,
+  CardContent,
+  ButtonGroup,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 class Tracking extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Tracking</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Typography
+          color="textSecondary"
+          variant="h4"
+          gutterBottom
+          component="h2"
+        >
+          Trackings
+        </Typography>
+
+        <Card>
+          <CardContent className="cardContent">
+            <Typography color="textSecondary" component="h5" variant="h5">
+              Where are you working today?
+            </Typography>
+            <ButtonGroup
+              color="primary"
+              aria-label="outlined primary button group"
+            >
+              <Button>Home</Button>
+              <Button>Office</Button>
+              <Button>Leave</Button>
+            </ButtonGroup>
+          </CardContent>
+        </Card>
       </div>
     );
   }

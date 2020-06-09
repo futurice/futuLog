3a58f1c21c30c69f1efe12d95d15da4bc934e305
Jan van Brügge <supermanitu@gmail.com>
2020-06-09 12:51:25 +0200

    Add endpoint to get more info about the user

---
+++ backend/src/Data/User.hs
@@ -0,0 +1,18 @@
+module Data.User where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Swagger (ToSchema)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+data User
+  = MkUser
+      { first_name :: Text,
+        last_name :: Text,
+        email :: Text,
+        portrait_full_url :: Text,
+        portrait_thumb_url :: Text,
+        portrait_badge_url :: Text
+      }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)

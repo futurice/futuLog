26d646862f169422b8a308b5af65586da9ddfe20
Egor Sorokin <>
2020-07-22 19:35:45 +0200

    Fix select

--- frontend/src/app/ui/playgroundPage/PlaygroundPage.tsx
+++ frontend/src/app/ui/playgroundPage/PlaygroundPage.tsx
@@ -3,17 +3,13 @@ import dayjs from "dayjs";
 import { PlanningCalendarDay } from "app/ui/planningPage/PlanningCalendarDay";
 import { Box } from "@material-ui/core";
 import { Workmode } from "app/services/apiClientService";
+import Select from '../ux/dropdown';
 
 export const PlaygroundPage: React.FC = () => {
   return (
     <div className="PlaygroundPage">
       <Box padding="1rem 0.5rem 2rem" border="1px solid grey">
-        <PlanningCalendarDay
-          date={dayjs().utc()}
-          workmode={Workmode.Home}
-          onSelectWorkmodes={(workmodes) => console.log("onSelectWorkmodes", workmodes)}
-          onClose={() => console.log("closed")}
-        />
+        <Select />
       </Box>
     </div>
   );
--- frontend/src/app/ui/ux/dropdown.tsx
+++ frontend/src/app/ui/ux/dropdown.tsx
@@ -1,117 +1,119 @@
 import React, { useState } from 'react';
-import { styled } from "@material-ui/core/styles";
-import { colors, Theme, theme } from "./theme";
-import Select, {SelectProps as MuiSelectProps} from '@material-ui/core/Select';
+import { styled } from '@material-ui/core/styles';
+import { Select as MuiSelect } from '@material-ui/core';
+import { SelectProps as MuiSelectProps } from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import MenuProps from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { colors, theme } from './theme';
+import { IconArrowUp } from './icons';
 
 
-export type SelectProps = MuiSelectProps;
-//export type MyMenuProps = MenuProps;
-
-export const DropDown = styled((props: MuiSelectProps) => <Select {...props} />)({
-    color: `${colors["deep-blue-80"]}`,
-    border: `1px solid ${colors["deep-blue-80"]}`,
-    boxSizing: 'border-box',
-    borderRadius: '4px',
-    "&:hover": {
-      border: '1px solid #6643EF',
-      backgroundColor: '#D2CEE3',
+export const DropDown = styled((props: MuiSelectProps) => <MuiSelect {...props} />)({
+  color: `${colors['deep-blue-80']}`,
+  border: `1px solid ${colors['deep-blue-80']}`,
+  boxSizing: 'border-box',
+  borderRadius: '4px',
+  '&:hover': {
+    border: '1px solid #6643EF',
+    backgroundColor: '#D2CEE3',
+  },
+  '&:active': {
+    backgroundColor: '#D2CEE3',
+  },
+  '&:disabled': {
+    background: 'rgba(255, 255, 255, 0.5)',
+    border: '1px solid rgba(166, 157, 199, 0.5)',
+    color: 'rgba(255, 255, 255, 0.5)'
+  },
+  '&.MuiSelect-icon': {
+    background: `${colors['deep-blue-80']}`,
+    color: `${colors['white']}`,
+  },
+  list: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    background: 'white',
+    '& li': {
+      fontWeight: 200,
+      paddingTop: 12,
+      paddingBottom: 12,
     },
-    "&:active": {
-      backgroundColor: '#D2CEE3',
+    '& li:hover': {
+      background: `${colors['deep-blue-80']}`
     },
-    "&:disabled":{
-      background: 'rgba(255, 255, 255, 0.5)',
-      border: '1px solid rgba(166, 157, 199, 0.5)',
-      color: 'rgba(255, 255, 255, 0.5)'
+    '& li.Mui-selected': {
+      color: 'white',
+      background: `${colors['deep-blue-50']}`
     },
-    "&.MuiSelect-icon":{
-      background: `${colors["deep-blue-80"]}`,
-      color: `${colors["white"]}`,
-    },
-    list: {
-      paddingTop:0,
-      paddingBottom:0,
-      background:'white',
-    "& li":{
-      fontWeight:200,
-      paddingTop:12,
-      paddingBottom:12,
-    },
-    "& li:hover":{
-      background: `${colors["deep-blue-80"]}`
-    },
-    "& li.Mui-selected":{
-      color:'white',
-      background: `${colors["deep-blue-50"]}`
-    },
-    "& li.Mui-selected:hover":{
-      background: `${colors["deep-blue-80"]}`
+    '& li.Mui-selected:hover': {
+      background: `${colors['deep-blue-80']}`
     }
   }
 });
 
 
 const useStyles = makeStyles({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-      color: `${colors["deep-blue-80"]}`,
-      border: `1px solid ${colors["deep-blue-80"]}`,
-      boxSizing: 'border-box',
-      borderRadius: '4px',
-      "&:hover": {
-        border: '1px solid #6643EF',
-        backgroundColor: '#D2CEE3',
-      },
-      "&:active": {
-        backgroundColor: '#D2CEE3',
-      },
-      "&:disabled":{
-        background: 'rgba(255, 255, 255, 0.5)',
-        border: '1px solid rgba(166, 157, 199, 0.5)',
-        color: 'rgba(255, 255, 255, 0.5)'
-      },
-      "&.MuiSelect-icon":{
-        background: `${colors["deep-blue-80"]}`,
-        color: `${colors["white"]}`,
-      },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    color: `${colors['deep-blue-80']}`,
+    border: `1px solid ${colors['deep-blue-80']}`,
+    boxSizing: 'border-box',
+    borderRadius: '4px',
+    '&:hover': {
+      border: '1px solid #6643EF',
+      backgroundColor: '#D2CEE3',
     },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
+    '&:active': {
+      backgroundColor: '#D2CEE3',
     },
-    list: {
-      paddingTop:0,
-      paddingBottom:0,
-      background:'white',
-      "& li":{
-        fontWeight:200,
-        paddingTop:12,
-        paddingBottom:12,
-      },
-      "& li:hover":{
-        background: `${colors["deep-blue-80"]}`
-      },
-      "& li.Mui-selected":{
-        color:'white',
-        background: `${colors["deep-blue-50"]}`
-      },
-      "& li.Mui-selected:hover":{
-        background: `${colors["deep-blue-80"]}`
-      },
+    '&:disabled': {
+      background: 'rgba(255, 255, 255, 0.5)',
+      border: '1px solid rgba(166, 157, 199, 0.5)',
+      color: 'rgba(255, 255, 255, 0.5)'
     },
-    icon:{
-      color: `${colors["deep-blue-50"]}`,
-      right: 12,
-      position: 'absolute',
-      userSelect: 'none',
-      pointerEvents: 'none'
+    '&.MuiSelect-icon': {
+      background: `${colors['deep-blue-80']}`,
+      color: `${colors['white']}`,
     },
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  list: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    background: 'white',
+    '& li': {
+      fontWeight: 200,
+      paddingTop: 12,
+      paddingBottom: 12,
+    },
+    '& li:hover': {
+      background: `${colors['deep-blue-80']}`
+    },
+    '& li.Mui-selected': {
+      color: 'white',
+      background: `${colors['deep-blue-50']}`
+    },
+    '& li.Mui-selected:hover': {
+      background: `${colors['deep-blue-80']}`
+    },
+  },
+  icon:{
+    color: `${colors["deep-blue-50"]}`,
+    right: 12,
+    position: 'absolute',
+    userSelect: 'none',
+    pointerEvents: 'none'
+  },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
 });
 
 // export interface entryData{
@@ -119,56 +121,70 @@ const useStyles = makeStyles({
 //   entryText: string[];
 // }
 
-export default function FutuSelect(){ //(entry: entryData, label: string){
-  const classes=useStyles()
-  const [val, setNewVal] = React.useState('');
+export default function Select() { //(entry: entryData, label: string){
+  const classes = useStyles()
   // const entryText = entry[0];
   // const entryValue = entry[1];
   // const entryLen = entryText.length;
-  const handleChange = (event:any) => {
-    setNewVal(event.target.value)
-  };
+
   // moves the menu below the select input
+
+  const [val, setVal] = React.useState<string | number>('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setVal(event.target.value as number);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   const menuProps = {
     classes: {
       list: classes.list
     },
     anchorOrigin: {
-      vertical: 1,//"bottom",
-        horizontal: 1,//"left"
+      vertical: 1,//'bottom',
+      horizontal: 1,//'left'
     },
     transformOrigin: {
-      vertical: 1, //"top",
-        horizontal: 1 //"left"
+      vertical: 1, //'top',
+      horizontal: 1 //'left'
     },
     getContentAnchorEl: null
   };
-  const iconComponent = (props:any) => {
-  return (
-    <ExpandMoreIcon className={props.className + " " + classes.icon}/>
-  )};
 
+
+  console.log('open:::', open ? 'true' : 'false');
 
   return (
     <div>
-    <FormControl className={classes.formControl}>
-      <InputLabel id="demo-simple-select-label">label</InputLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={val}
-        onChange={handleChange}
-        IconComponent={iconComponent}
-        MenuProps= {menuProps}
-      >
-      <MenuItem value="">
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">label</InputLabel>
+        <DropDown
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          value={val}
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          onChange={handleChange}
+          IconComponent={() => (<IconArrowUp color={colors['deep-blue-80']}/>)}
+          MenuProps={menuProps}
+        >
+          <MenuItem value="">
             <em>None</em>
           </MenuItem>
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
-      </Select>
-    </FormControl>
+        </DropDown>
+      </FormControl>
     </div>
   );
 }

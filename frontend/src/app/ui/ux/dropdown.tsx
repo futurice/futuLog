f05517f409a21ea7aef2c33cf2d805a494e21c57
Patrick Burden <patrick.burden@futurice.com>
2020-07-27 15:19:58 +0200

    expected requested changes based off previous requests

--- frontend/src/app/ui/ux/dropdown.tsx
+++ frontend/src/app/ui/ux/dropdown.tsx
@@ -1,64 +1,63 @@
-import React, { useState } from 'react';
-import { styled } from '@material-ui/core/styles';
-import { Select as MuiSelect } from '@material-ui/core';
-import { SelectProps as MuiSelectProps } from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import { makeStyles } from '@material-ui/core/styles';
-
-import { colors, theme } from './theme';
-import { IconArrowUp } from './icons';
+import React, { useState } from "react";
+import { styled } from "@material-ui/core/styles";
+import { Select as MuiSelect } from "@material-ui/core";
+import { SelectProps as MuiSelectProps } from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import { makeStyles } from "@material-ui/core/styles";
+import { colors, theme } from "./theme";
+import { IconArrowUp } from "./icons";
 
 
 export const DropDown = styled((props: MuiSelectProps) => <MuiSelect {...props} />)({
-  color: `${colors['deep-blue-80']}`,
-  border: `1px solid ${colors['deep-blue-80']}`,
-  boxSizing: 'border-box',
-  borderRadius: '4px',
-  backgroundColor: `${colors['deep-blue-80']}`,
-  '&:hover': {
-    border: '1px solid #6643EF',
-    backgroundColor: `${colors['deep-blue-50']}`,
+  color: `${colors["deep-blue-80"]}`,
+  border: `1px solid ${colors["deep-blue-80"]}`,
+  boxSizing: "border-box",
+  borderRadius: "4px",
+  backgroundColor: `${colors["deep-blue-80"]}`,
+  "&:hover": {
+    border: "1px solid #6643EF",
+    backgroundColor: `${colors["deep-blue-50"]}`,
   },
-  '&:active': {
-    backgroundColor: `${colors['deep-blue-50']}`,
+  "&:active": {
+    backgroundColor: `${colors["deep-blue-50"]}`,
   },
-  '&:disabled': {
-    background: 'rgba(255, 255, 255, 0.5)',
-    border: '1px solid rgba(166, 157, 199, 0.5)',
-    color: 'rgba(255, 255, 255, 0.5)',
-    backgroundColor: 'rgba(32, 10, 116, 0.5)',
+  "&:disabled": {
+    background: "rgba(255, 255, 255, 0.5)",
+    border: "1px solid rgba(166, 157, 199, 0.5)",
+    color: "rgba(255, 255, 255, 0.5)",
+    backgroundColor: "rgba(32, 10, 116, 0.5)",
   },
-  '&.MuiSelect-icon': {
-    background: `${colors['deep-blue-80']}`,
-    color: `${colors['white']}`,
+  "&.MuiSelect-icon": {
+    background: `${colors["deep-blue-80"]}`,
+    color: `${colors["white"]}`,
   },
   list: {
     paddingTop: 0,
     paddingBottom: 0,
-    background: 'white',
-    '& li': {
+    background: `${colors["white"]}`,
+    "& li": {
       fontWeight: 200,
       paddingTop: 12,
       paddingBottom: 12,
     },
-    '& li:hover': {
-      background: `${colors['deep-blue-80']}`
+    "& li:hover": {
+      background: `${colors["deep-blue-80"]}`
     },
-    '& li.Mui-selected': {
-      color: 'white',
-      background: `${colors['deep-blue-50']}`
+    "& li.Mui-selected": {
+      color: `${colors["white"]}`,
+      background: `${colors["deep-blue-50"]}`
     },
-    '& li.Mui-selected:hover': {
-      background: `${colors['deep-blue-80']}`
+    "& li.Mui-selected:hover": {
+      background: `${colors["deep-blue-80"]}`
     }
   },
   icon: {
-    background: `${colors['deep-blue-80']}`,
+    background: `${colors["deep-blue-80"]}`,
   },
-  '& .MuiSelect-selectMenu':{
-    backgroundColor: 'white'
+  "& .MuiSelect-selectMenu":{
+    backgroundColor: `${colors["white"]}`,
   }
 });
 
@@ -67,25 +66,25 @@ const useStyles = makeStyles({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-    color: `${colors['deep-blue-80']}`,
-    border: `1px solid ${colors['deep-blue-80']}`,
-    boxSizing: 'border-box',
-    borderRadius: '4px',
-    '&:hover': {
-      border: '1px solid #6643EF',
-      backgroundColor: '#D2CEE3',
+    color: `${colors["deep-blue-80"]}`,
+    border: `1px solid ${colors["deep-blue-80"]}`,
+    boxSizing: "border-box",
+    borderRadius: "4px",
+    "&:hover": {
+      border: `1px solid ${colors["deep-blue-50"]}`,
+      backgroundColor: `${colors["deep-blue-80"]}`,
     },
-    '&:active': {
-      backgroundColor: '#D2CEE3',
+    "&:active": {
+      backgroundColor: `${colors["deep-blue-80"]}`,
     },
-    '&:disabled': {
-      background: 'rgba(255, 255, 255, 0.5)',
-      border: '1px solid rgba(166, 157, 199, 0.5)',
-      color: 'rgba(255, 255, 255, 0.5)'
+    "&:disabled": {
+      background: "rgba(255, 255, 255, 0.5)",
+      border: "1px solid rgba(166, 157, 199, 0.5)",
+      color: "rgba(255, 255, 255, 0.5)"
     },
-    '&.MuiSelect-icon': {
-      background: `${colors['deep-blue-80']}`,
-      color: `${colors['white']}`,
+    "&.MuiSelect-icon": {
+      background: `${colors["deep-blue-80"]}`,
+      color: `${colors["white"]}`,
     },
   },
   selectEmpty: {
@@ -94,38 +93,37 @@ const useStyles = makeStyles({
   list: {
     paddingTop: 0,
     paddingBottom: 0,
-    background: 'white',
-    '& li': {
+    background: `${colors["white"]}`,
+    "& li": {
       fontWeight: 200,
       paddingTop: 12,
       paddingBottom: 12,
     },
-    '& li:hover': {
-      color: 'white',
-      background: `${colors['deep-blue-80']}`
+    "& li:hover": {
+      color: `${colors["white"]}`,
+      background: `${colors["deep-blue-80"]}`
     },
-    '& li.Mui-selected': {
-      color: 'white',
-      background: `${colors['deep-blue-50']}`
+    "& li.Mui-selected": {
+      color: `${colors["white"]}`,
+      background: `${colors["deep-blue-50"]}`
     },
-    '& li.Mui-selected:hover': {
-      color: 'white',
-      background: `${colors['deep-blue-80']}`
+    "& li.Mui-selected:hover": {
+      color: `${colors["white"]}`,
+      background: `${colors["deep-blue-80"]}`
     },
   },
   icon:{
     color: `${colors["deep-blue-50"]}`,
     right: 12,
-    position: 'absolute',
-    userSelect: 'none',
-    pointerEvents: 'none',
-    background: `${colors['deep-blue-80']}`,
+    position: "absolute",
+    userSelect: "none",
+    pointerEvents: "none",
+    background: `${colors["deep-blue-80"]}`,
   },
   button: {
-    display: 'block',
+    display: "block",
     marginTop: theme.spacing(2),
   },
-
 });
 
 export interface entryData{
@@ -138,7 +136,6 @@ function buildMenuItems(entry:entryData[]){
     entry.map((entrySet,index) => {
       return (<MenuItem value={entrySet.entryValue}>{entrySet.entryText}</MenuItem>)
     })
-
   )
 }
 
@@ -147,41 +144,35 @@ interface selectProps{
   label: string
 }
 
-export const Select = ({entry, label}: selectProps) => { //(entry: entryData, label: string)
+export const Select = ({entry, label}: selectProps) => {
   const classes = useStyles()
-
-  const [val, setVal] = React.useState<string | number>('');
+  const [val, setVal] = React.useState<string | number>("");
   const [open, setOpen] = React.useState(false);
-
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setVal(event.target.value as number);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
-
   const handleOpen = () => {
     setOpen(true);
   };
-
   const menuProps = {
     classes: {
       list: classes.list
     },
     anchorOrigin: {
-      vertical: 1,//'bottom',
-      horizontal: 1,//'left'
+      vertical: 1,
+      horizontal: 1,
     },
     transformOrigin: {
-      vertical: 1, //'top',
-      horizontal: 1 //'left'
+      vertical: 1,
+      horizontal: 1,
     },
     getContentAnchorEl: null,
   };
 
-
-  console.log('open:::', open ? 'true' : 'false');
+  console.log("open:::", open ? "true" : "false");
 
   return (
     <div>

2c830c12ed68d9d91e93c2bb833661beaec0131d
pbur <patrick.burden@futurice.com>
2020-08-03 12:38:30 +0200

    potential rough outline of searchbox functionality

---
+++ frontend/src/app/ui/ux/searchbox.tsx
@@ -0,0 +1,196 @@
+import React from "react";
+import { styled } from "@material-ui/core/styles";
+import { Select as MuiSelect } from "@material-ui/core";
+import { SelectProps as MuiSelectProps } from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+
+import {InputBaseProps as MuiInputBaseProps} from "@material-ui/core/InputBase";
+
+import { colors } from "./theme";
+import { Flex } from "./containers";
+import { InputBase } from "./inputs"
+import { AutocompleteProps as MuiAutocompleteProps } from "@material-ui/lab/Autocomplete";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { IconSearch } from "./icons";
+
+type ISelectProps = MuiSelectProps;
+
+export const StyledSearch = styled((props: MuiAutocompleteProps) => <Autocomplete {...props} />)({
+  color: `${colors["deep-blue-80"]}`,
+  border: "none",
+  backgroundColor: colors.white,
+  borderRadius: "4px",
+
+  transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms," +
+    "box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms," +
+    "border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+
+  "& .MuiInputBase-input": {
+    padding: "8px 40px 8px 8px",
+    border: `2px solid ${colors["deep-blue-80"]}`,
+    borderRadius: "4px",
+
+    transition: "box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms," +
+      "border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+  },
+
+  "& .MuiSelect-icon": {
+    width: "36px",
+    height: "37px",
+    top: "unset",
+    right: "0",
+
+    justifyContent: "center",
+    alignItems: "center",
+
+    border: `2px solid ${colors["deep-blue-80"]}`,
+    backgroundColor: colors["deep-blue-80"],
+    borderTopRightRadius: "5px",
+    borderBottomRightRadius: "5px",
+
+    transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms," +
+      "border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+  },
+
+  "&:hover .MuiInputBase-input": {
+    border: `2px solid ${colors["deep-blue-50"]}`,
+    boxShadow: "2px 2px 4px rgba(10, 3, 37, 0.2)",
+  },
+
+  "&:hover .MuiSelect-icon": {
+    backgroundColor: colors["deep-blue-50"],
+    border: `2px solid ${colors["deep-blue-50"]}`,
+  },
+
+  "&.Mui-focused": {
+    "& .MuiInputBase-input": {
+      border: `2px solid ${colors["deep-blue-50"]}`,
+      boxShadow: "2px 2px 4px rgba(10, 3, 37, 0.2)",
+      backgroundColor: colors.white,
+    },
+
+    "& .MuiSelect-icon": {
+      border: `2px solid ${colors["deep-blue-50"]}`,
+    }
+  },
+
+  "&.Mui-disabled": {
+    opacity: 0.5,
+
+    "&:hover": {
+      "& .MuiInputBase-input": {
+        border: `2px solid ${colors["deep-blue-80"]}`,
+        boxShadow: "none",
+      },
+      "& .MuiSelect-icon": {
+        backgroundColor: colors["deep-blue-80"],
+        border: `2px solid ${colors["deep-blue-80"]}`,
+      },
+    }
+  },
+
+  "& .MuiSelect-root": {
+    boxSizing: "border-box",
+    width: "200px",
+  }
+});
+
+const useStyles = makeStyles({
+  list: {
+    paddingTop: 0,
+    paddingBottom: 0,
+
+    backgroundColor: colors.white,
+    borderRadius: "4px",
+    border: `1px solid ${colors["deep-blue-80"]}`,
+
+    "& option": {
+      fontWeight: 200,
+      padding: "10px 8px",
+      minWidth: "198px",
+
+      transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms," +
+        "border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    },
+
+    "& option:last-child": {
+      borderRadius: "0px 0px 4px 4px",
+    },
+    "& option:first-child": {
+      borderRadius: "4px 4px 0px 0px",
+    },
+
+    "& option:hover": {
+      backgroundColor: colors["deep-blue-20"],
+      color: colors["deep-blue-80"],
+    },
+
+    "& option:active": {
+      fontWeight: "bold",
+    },
+
+    "& option.Mui-selected": {
+      color: `${colors["deep-blue-80"]}`,
+      backgroundColor: `${colors.white}`,
+      fontWeight: "bold",
+    },
+
+    "& option.Mui-selected:hover": {
+      color: `${colors["deep-blue-80"]}`,
+      backgroundColor: `${colors["deep-blue-20"]}`,
+    },
+  },
+});
+
+
+export const Search: React.FC<ISelectProps> = ({
+  children,
+  ...props
+}) => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false);
+
+  const menuProps = {
+    classes: {
+      list: classes.list
+    },
+    anchorOrigin: {
+      vertical: 40,
+      horizontal: 0,
+    },
+    transformOrigin: {
+      vertical: 0,
+      horizontal: 0,
+    },
+    getContentAnchorEl: null,
+    border: `1px solid ${colors["deep-blue-80"]}`,
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+      <StyledSearch
+        id="combo-box-demo"
+        options={children}
+        getOptionLabel={(option) => option.title}
+        style={{ width: 300 }}
+        open={open}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        IconComponent={(
+            <Flex className={"MuiSelect-icon"}>
+              <IconSearch/>
+            </Flex>
+          )}
+        renderInput={(params) => <InputBase{...params} />}
+        MenuProps={menuProps}
+        {...props}
+      />
+    );
+}

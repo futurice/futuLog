a8846aceb39a059b6a4b1c7445b4ffaca497c9e7
Jan van Brügge <jan.van.brugge@futurice.com>
2021-05-19 11:36:01 +0200

    Make ca folder configurable

--- startup.sh
+++ startup.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-if [ -e /run/secrets/ca.crt ]; then
-    cp /run/secrets/ca.crt /usr/local/share/ca-certificates/
+secrets=/run/secrets/ca.crt
+
+if [ ! -z "$CA_FOLDER" ]; then
+    secrets="$CA_FOLDER/ca.crt"
+fi
+
+if [ -e "$secrets" ]; then
+    cp "$secrets" /usr/local/share/ca-certificates/
     update-ca-certificates
+else
+    echo "No certificates found, proceeding"
 fi
 
 ./office-tracker
